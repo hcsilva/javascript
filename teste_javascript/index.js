5830635bd99a2b8ccce68d1e0add35ed0d17ecdd
henrique <henriquec.dasilva@hotmail.com>
2018-08-04 10:27:29 -0300

    teste com javascript

---
+++ teste_javascript/index.js
@@ -0,0 +1,8 @@
+let n = 7;
+//let novo recurso, usado para setar a variável dentro de um bloco
+
+for (let i = 0; i <= 10; i++) {
+    //template string, novo recurso do java script.
+    console.log(`${i} X ${n} = ${i*n}`);
+    
+}
