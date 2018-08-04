bcb84737a347419c7e53dbfd6323728c3e2eceb4
henrique <henriquec.dasilva@hotmail.com>
2018-08-04 11:29:55 -0300

    tests of events in javascript

---
+++ teste_javascript/indexeven.js
@@ -0,0 +1,9 @@
+//manipulamos a aplicação
+window.addEventListener('focus', event => {
+    console.log('focus');
+});
+
+
+document.addEventListener('click', event => {
+    console.log('click');
+});
